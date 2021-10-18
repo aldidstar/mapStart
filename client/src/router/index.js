aca98274236f12711476176809861cc83b82ac5e
aldidstar <aldidestaryana96@gmail.com>
2021-10-18 21:53:03 +0700

    chart done

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,36 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import Line from '../views/Line.vue'
-import GMap from '../views/GMap.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import GMap from "../views/GMap.vue";
 
 const routes = [
-  
   {
-    path: '/register',
-    name: 'Register', 
-    component: Register
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
   {
-    path: '/',
-    name: 'Login',
-    component: Login
+    path: "/",
+    name: "Login",
+    component: Login,
   },
-  
+
   {
-    path: '/map',
-    component: GMap
+    path: "/map",
+    component: GMap,
   },
-  
-  {
-    path: '/line',
-    component: Line
-  }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
